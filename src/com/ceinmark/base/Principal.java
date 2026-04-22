a05d248609c9a0976a514ff9f820e7de6f140feb
moises <moises@pcita>
2026-04-22 13:10:34 +0200

    Actualización de la base

--- src/com/ceinmark/base/Principal.java
+++ src/com/ceinmark/base/Principal.java
@@ -3,16 +3,27 @@ package com.ceinmark.base;
 import java.util.Scanner;
 
 public class Principal {
-	
+
 	public static void main(String[] args) {
 		Scanner ent = new Scanner(System.in);
-		
-		
+
 		System.out.println("Elige qué operación quieres realizar: SUMA / RESTA / MULTIPLICACIÓN / DIVISIÓN");
 		String operacion = ent.nextLine();
-		
-		
-	}//fin main
+		switch (operacion) {
+		case "SUMA":
+			break;
+		case "RESTA":
+			break;
+		case "MULTIPLICACIÓN":
+			break;
+		case "DIVISIÓN":
+			break;
+		default:
+			System.out.println("Esta opción no está disponible");
+		}// fin switch case
+
+	}// fin main
+
 	
-	
+
 }
