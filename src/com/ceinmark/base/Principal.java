5dcfe965d863146ea2b6400aede05582e9d02e9d
moises <moises@pcita>
2026-04-22 13:00:19 +0200

    Base del proyecto

---
+++ src/com/ceinmark/base/Principal.java
@@ -0,0 +1,18 @@
+package com.ceinmark.base;
+
+import java.util.Scanner;
+
+public class Principal {
+	
+	public static void main(String[] args) {
+		Scanner ent = new Scanner(System.in);
+		
+		
+		System.out.println("Elige qué operación quieres realizar: SUMA / RESTA / MULTIPLICACIÓN / DIVISIÓN");
+		String operacion = ent.nextLine();
+		
+		
+	}//fin main
+	
+	
+}
